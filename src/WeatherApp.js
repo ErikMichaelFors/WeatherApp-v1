241220064485a0f6124f2918dbb375ac6a444711
Michael Fors <erik.michael.fors@gmail.com>
2019-04-12 06:56:21 +0200

    First changes

---
+++ src/WeatherApp.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+//import logo from './logo.svg';
+//import './App.css';
+import './myOwnStyles.css';
+
+class WeatherApp extends Component {
+  render() {
+    return (
+      <div className="WeatherApp">
+      HALLÅ
+      </div>
+    );
+  }
+}
+
+export default WeatherApp;
